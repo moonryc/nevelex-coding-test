d1993ffe7fa289633df1fc3f7a65d8da55956fd9
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-05 21:20:20 -0500

    set up new animal to create new animals

--- src/pages/NewAnimal.tsx
+++ src/pages/NewAnimal.tsx
@@ -1,8 +1,9 @@
-import React, {ChangeEvent, FormEvent, useReducer, useState} from 'react';
+import React, {ChangeEvent, FormEvent, useState} from 'react';
 import Paper from "../components/paper";
 import Wrapper from "../components/wrapper";
 import {IAnimal} from "../types";
 import {useCreateAnimal} from "../hooks";
+import {useNavigate} from "react-router-dom";
 
 const initialAnimalData: IAnimal = {
     commonName: '',
@@ -12,11 +13,14 @@ const initialAnimalData: IAnimal = {
 }
 
 const NewAnimal = () => {
-
-    const {createAnimal,error} = useCreateAnimal()
-
+    const navigate = useNavigate();
+    const {createAnimal, error} = useCreateAnimal()
     const [animalFormData, setAnimalFormData] = useState<IAnimal>(initialAnimalData);
     const {commonName, scientificName, family, imageURL} = animalFormData
+
+    const toHome = () => {
+      navigate('/')
+    }
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setAnimalFormData(prevState => ({...prevState, [e.target.name]: e.target.value}))
@@ -31,29 +35,38 @@ const NewAnimal = () => {
         <Wrapper>
             <Paper>
                 <section>
-                <form onSubmit={(e)=>handleSubmit(e)}>
-                    <div>
-                        <label htmlFor={'commonName'}>Common Name:</label>
-                        <input onChange={(e) => handleChange(e)} name={'commonName'} value={commonName}
-                               placeholder={'commonName'}/>
+                    <div className={'input-container'}>
+                        <button className={'menu-button'} onClick={toHome}>BACK</button>
                     </div>
-                    <div>
-                        <label htmlFor={'scientificName'}>Scientific Name:</label>
-                        <input onChange={(e) => handleChange(e)} name={'scientificName'} value={scientificName}
-                               placeholder={'scientificName'}/>
-                    </div>
-                    <div>
-                        <label htmlFor={'family'}>Family:</label>
-                        <input onChange={(e) => handleChange(e)} name={'family'} value={family} placeholder={'family'}/>
-                    </div>
-                    <div>
-                        <label htmlFor={'imageURL'}>URL image:</label>
-                        <input onChange={(e) => handleChange(e)} name={'imageURL'} value={imageURL}
-                               placeholder={'imageURL'}/>
-                    </div>
-                    <button>Submit</button>
-                </form>
-                {error && (error)}
+                    <form className={'new-animal-form'} onSubmit={(e) => handleSubmit(e)}>
+                        <div>
+                            <label htmlFor={'commonName'}>Common Name:</label>
+                            <input
+                                onChange={(e) => handleChange(e)}
+                                name={'commonName'} value={commonName}
+                                placeholder={'Common Name'}/>
+                        </div>
+                        <div>
+                            <label htmlFor={'scientificName'}>Scientific Name:</label>
+                            <input
+                                onChange={(e) => handleChange(e)} name={'scientificName'} value={scientificName}
+                                placeholder={'Scientific Name'}/>
+                        </div>
+                        <div>
+                            <label htmlFor={'family'}>Family:</label>
+                            <input
+                                onChange={(e) => handleChange(e)} name={'family'} value={family}
+                                placeholder={'Family'}/>
+                        </div>
+                        <div>
+                            <label htmlFor={'Image URL'}>URL image:</label>
+                            <input
+                                onChange={(e) => handleChange(e)} name={'imageURL'} value={imageURL}
+                                placeholder={'Image URL'}/>
+                        </div>
+                        <button className={'menu-button'}>Submit</button>
+                    </form>
+                    {error && (error)}
                 </section>
             </Paper>
         </Wrapper>
