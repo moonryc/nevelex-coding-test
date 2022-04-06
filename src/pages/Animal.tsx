e8764dfe7bb569e0438af4e93ea360adc054188f
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-05 21:19:08 -0500

    display a specific animal when you click on it. nothing is displayed while it is loading the data. this is done to keep the user from seeing old data

--- src/pages/Animal.tsx
+++ src/pages/Animal.tsx
@@ -11,7 +11,7 @@ interface props {}
 const Animal:React.FC<props> = () => {
     const params = useParams()
     const navigate = useNavigate()
-    const {fetchAnimalById} = useGetAnimalById()
+    const {fetchAnimalById,loading} = useGetAnimalById()
     const {selectedAnimal} = useAnimalsContext()
 
     const toHomepage = () => {
@@ -24,48 +24,25 @@ const Animal:React.FC<props> = () => {
         }
     },[])
 
-    useEffect(() => {
-        console.log(selectedAnimal)
-    }, [selectedAnimal]);
-
-    const currentBreakpoint = useCurrentBreakpoint();
-    const [isMinLarge, setIsMinLarge] = useState(true);
-
-    /**
-     *  sets the isMinLarge state to true if the current breakpoint is large or extra large
-     */
-    useEffect(() => {
-        //resize the section based on the breakpoint
-        if (['xs', 'sm', 'md'].includes(currentBreakpoint)) {
-            setIsMinLarge(false)
-        } else {
-            setIsMinLarge(true)
-        }
-    }, [currentBreakpoint])
-
-
     return (
         <Wrapper>
             <Paper>
-                <section>
-                <button onClick={toHomepage}>Back</button>
+                {!loading &&
+                (<section>
+                    <div className={'input-container'}>
+                        <button className={'menu-button'} onClick={toHomepage}>BACK</button>
+                    </div>
                 {selectedAnimal && (<>
                 <img alt={selectedAnimal.commonName} src={selectedAnimal.imageURL} className={'imageStyle'}/>
                 <aside style={{flex: 1}}>
                     <h1 className={'headerStyle'}>
                         {selectedAnimal.commonName.toUpperCase()}
                     </h1>
-                    <p className={'paragraphStyle'} style={{
-                        marginLeft: isMinLarge ? '40px' : '0px',
-                        textAlign: isMinLarge ? 'left' : 'center',
-                    }}>Scientific name: {selectedAnimal.scientificName}</p>
-                    <p className={'paragraphStyle'} style={{
-                        marginLeft: isMinLarge ? '40px' : '0px',
-                        textAlign: isMinLarge ? 'left' : 'center',
-                    }}>Family: {selectedAnimal.family}</p>
+                    <p className={'paragraph-style'}>Scientific name: {selectedAnimal.scientificName}</p>
+                    <p className={'paragraphStyle'} >Family: {selectedAnimal.family}</p>
                 </aside>
                 </>)}
-                </section>
+                </section>)}
             </Paper>
         </Wrapper>
     );
