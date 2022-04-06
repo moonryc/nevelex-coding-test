962c1f6aff64d3a8ef2d4df6adc913fabc824261
moonryc <70920813+moonryc@users.noreply.github.com>
2022-04-05 21:19:26 -0500

    optimized animal search

--- src/components/animal-search/index.tsx
+++ src/components/animal-search/index.tsx
@@ -1,32 +1,28 @@
 import React from 'react';
-// import { StudentData } from '../../contexts/studentDataContext';
 import './animalSearchStyles.css'
 import {useAnimalsContext} from "../../context/AnimalContextContainer";
-import {useNavigate} from "react-router-dom";
+import IndividualAnimal from "./IndividualAnimal";
+
 
 interface props {
     isDeleteMode: boolean
 }
 
 const AnimalSearch: React.FC<props> = ({isDeleteMode}) => {
-
     const {filteredAnimalData} = useAnimalsContext()
-    const navigate = useNavigate()
-
     return (
         <section data-testid="display-students">
-            {filteredAnimalData && (filteredAnimalData.map((animal, index) =>
-                    <article key={animal.commonName + index} className={'animalStyle'}>
-                        <img alt={animal.commonName} onClick={()=>navigate(`/${animal.id}`)} src={animal.imageURL} className={'imageStyle'}/>
-                        <aside style={{flex: 1}}>
-                            <h1 className={'headerStyle'}>
-                                {animal.commonName.toUpperCase()}
-                            </h1>
-                        </aside>
-                        {/*//@ts-ignore*/}
-                        {isDeleteMode && animal.id.toString().length > 1 ? <button>Delete</button> : <></>}
-                    </article>)
-            )}
+            {filteredAnimalData && (filteredAnimalData.map((animal, index) => {
+                return (
+                    <>
+                        {!isDeleteMode && (
+                            <IndividualAnimal animal={animal} index={index} isDeleteMode={isDeleteMode}/>)}
+                        {/*@ts-ignore*/}
+                        {isDeleteMode && animal.id.toString().length > 1 ?
+                            <IndividualAnimal animal={animal} index={index} isDeleteMode={isDeleteMode}/> : <></>}
+                    </>
+                )
+            }))}
         </section>
 
     )
